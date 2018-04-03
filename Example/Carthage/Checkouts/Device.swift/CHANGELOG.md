1f9e32dd986c385663488822f94a620aa3b4658a
jwelton122@googlemail.com <jakewelton@Jakes-MacBook-Pro.local>
2018-04-03 11:16:29 +0100

    Add example app

---
+++ Example/Carthage/Checkouts/Device.swift/CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+
+## 1.1
+
+- **Add**: Support for iPad Pro 10.5
+
+## 1.0
+
+- **Add**: iPhone 8, iPhone 8 Plus, iPhone X
+
+## 0.5.2
+
+- **Update**: Update project settings to Swift 4
---
+++ Example/example-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  example-app
+//
+//  Created by Jake Welton on 03/04/2018.
+//  Copyright © 2018 Mitek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
